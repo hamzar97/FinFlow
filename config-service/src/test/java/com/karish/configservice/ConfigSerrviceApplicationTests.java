89b207cbfb4afe2cba5a8bcccf095a893c74ee09
Hamza Bin Raza <hamzabinraza@Hamzas-MacBook-Air.local>
2025-04-10 19:18:12 +0500

    initial commit

---
+++ config-service/src/test/java/com/karish/configservice/ConfigSerrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karish.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSerrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gateway-service/src/test/java/com/karish/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karish.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
